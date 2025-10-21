b67e962bcdb50c3bac78eeeac50f43028d824653
Xentel <giginya2000@gmail.com>
2025-10-21 22:56:15 +0300

    21.10.2025 22.56

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Environment;
+
+public class GameManager : MonoBehaviour
+{
+
+    private World World = new World();
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
